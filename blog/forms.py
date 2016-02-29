fb079e338a82944397aea2828ddc3d318395214e
Tatochkin <kovalyova.tanya@gmail.com>
2016-02-29 20:58:48 +0200

    Fixing comments.

--- blog/forms.py
+++ blog/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Post, Comment
+from .models import Post
 
 class PostForm(forms.ModelForm):
 
@@ -7,8 +7,3 @@ class PostForm(forms.ModelForm):
         model = Post
         fields = ('title', 'text',)
 
-class CommentForm(forms.ModelForm):
-
-    class Meta:
-        model = Comment
-        fields = ('author', 'text',)
--- blog/views.py
+++ blog/views.py
@@ -4,7 +4,7 @@ from .models import Post
 from django.shortcuts import render, get_object_or_404
 from .forms import PostForm
 from django.shortcuts import redirect
-from .forms import PostForm, CommentForm
+
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
@@ -70,3 +70,14 @@ def add_comment_to_post(request, pk):
     else:
         form = CommentForm()
     return render(request, 'blog/add_comment_to_post.html', {'form': form})
+
+def comment_approve(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    comment.approve()
+    return redirect('blog.views.post_detail', pk=comment.post.pk)
+
+def comment_remove(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    post_pk = comment.post.pk
+    comment.delete()
+    return redirect('blog.views.post_detail', pk=post_pk)
